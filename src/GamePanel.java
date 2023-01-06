07540babce596957bfde023b00024041c500df64
DanioFiore <daniofiore@hotmail.com>
2023-01-06 14:09:33 +0100

    completed the keyPressed method so now we can control the snake

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -116,6 +116,10 @@ public class GamePanel extends JPanel implements ActionListener{
         if(y[0] > SCREEN_HEIGHT) {
             running = false;
         }
+
+        if(!running) {
+            timer.stop();
+        }
     }
 
     public void gameOver(Graphics g) {
@@ -138,7 +142,28 @@ public class GamePanel extends JPanel implements ActionListener{
     public class MyKeyAdapter extends KeyAdapter {
         @Override
         public void keyPressed(KeyEvent e) {
-
+            switch(e.getKeyCode()) {
+                case KeyEvent.VK_LEFT:
+                    if(direction != 'R') {
+                        direction = 'L';
+                    }
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    if(direction != 'L') {
+                        direction = 'R';
+                    }
+                    break;
+                case KeyEvent.VK_UP:
+                    if(direction != 'D') {
+                        direction = 'U';
+                    }
+                    break;
+                case KeyEvent.VK_DOWN:
+                    if(direction != 'U') {
+                        direction = 'D';
+                    }
+                    break;
+            }
         }
     }
     
